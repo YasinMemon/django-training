a46440a4f1225c31a17b698eff5d0be570a6c5c7
YasinMemon <memonyasin2007@gmail.com>
2025-06-05 23:35:59 +0530

    skfsj

--- base/views.py
+++ base/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, world! This is the home page.")
+    return render(request, 'base/index.html')    
