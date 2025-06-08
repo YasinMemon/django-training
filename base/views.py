4ab217ac82b70ead1ce0cb8e84f8d42836d23948
YasinMemon <memonyasin2007@gmail.com>
2025-06-08 20:15:00 +0530

    kdjsk

--- base/views.py
+++ base/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 
+def loginPage(request):
+    context = {}
+    return render(request, "base/login_register.html", context)
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) |
+                                Q(name__icontains=q) |
+                                Q(description__icontains=q))
     topics = Topic.objects.all()
-    return render(request, 'base/index.html', {'rooms': rooms, 'topics': topics})
+    room_count = rooms.count()
+    return render(request, 'base/index.html', {'rooms': rooms, 'topics': topics, 'room_count': room_count})
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
